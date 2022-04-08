85d375e9c2010ee19ca8d29a4e84c61a4a89fc14
WEBDERNargor <96941642+WEBDERNargor@users.noreply.github.com>
2022-04-08 16:25:41 +0700

    main

--- client.lua
+++ client.lua
@@ -1,74 +1,37 @@
--- Citizen.CreateThread(function()
--- 	while true do
+Citizen.CreateThread(function()
+	while true do
+        local name=GetPlayerName(PlayerId())
 
--- 		SetDiscordAppId(954902832484601926) -- ต้องแก้
+        local onlinePlayers = 0
+for i = 0, 255 do
+    if NetworkIsPlayerActive(i) then
+        onlinePlayers = onlinePlayers+1
+    end
+end
 
 
--- 		SetDiscordRichPresenceAsset('mafia1024') -- ต้องแก้
+		SetDiscordAppId(client_id) -- ต้องแก้
 
---         SetDiscordRichPresenceAssetText('Mafia City SS1') -- ต้องแก้
+
+		SetDiscordRichPresenceAsset('asset_name1024') -- ต้องแก้
+
+        SetDiscordRichPresenceAssetText('ชื่อที่จะให้แสดง') -- ต้องแก้
        
 
---         SetDiscordRichPresenceAssetSmall('mafia512') -- ต้องแก้
+        SetDiscordRichPresenceAssetSmall('asset_name512') -- ต้องแก้
 
 
---         SetDiscordRichPresenceAssetSmallText('Mafia City SS1') -- ต้องแก้
+        SetDiscordRichPresenceAssetSmallText('ชื่อที่จะให้แสดงตัวเล็ก') -- ต้องแก้
 
--- 		SetDiscordRichPresenceAction(0, "เข้าเซิฟเวอร์", "fivem://connect/localhost:30120")
---         SetDiscordRichPresenceAction(1, "เว็บไซต์", "้https://www.maifacityss1.com")
--- 		Citizen.Wait(60000)
--- 	end
--- end)
+		SetDiscordRichPresenceAction(0, "Join Server", "fivem://connect/localhost:30120")
+        SetDiscordRichPresenceAction(1, "Join discord", "discord_url")
+         Citizen.Wait(20*1000)
+        SetDiscordRichPresenceAction(1, "website", "https://nargor.net/")
 
 
-Citizen.CreateThread(function()
-    while true do
-        
-        --properties
-        local name = GetPlayerName(PlayerId())
-        local id = GetPlayerServerId(PlayerId())
-        local street = GetStreetNameFromHashKey(GetStreetNameAtCoord(table.unpack(GetEntityCoords(player))))
-        local vehicle = GetLabelText(GetDisplayNameFromVehicleModel(GetEntityModel(GetVehiclePedIsUsing(PlayerPedId()))))
-        local MPH = math.ceil(GetEntitySpeed(GetVehiclePedIsUsing(PlayerPedId())) * 2.236936)
-        local NumberOfPlayers = 0
-
-        for i = 0, 255 do
-            if NetworkIsPlayerActive(i) then
-                NumberOfPlayers = NumberOfPlayers + 1
-            end
-        end
-
-        --game status app
-        SetDiscordAppId(954902832484601926)
-
-        --logo icon
-        SetDiscordRichPresenceAsset('mafia1024')
-        SetDiscordRichPresenceAssetText('Mafia City SS1')
-
-        --buttons
-        
-        SetDiscordRichPresenceAction(0, "Join Server", "fivem://connect/localhost:30120")
-		SetDiscordRichPresenceAction(1, "Join Discord", "https://discord.gg/ejqqUB9SBT")
-		Wait(20*1000)
-        SetDiscordRichPresenceAction(1, "Website", "https://mafiacityss1.com")
-
-        --small icon
-        --SetDiscordRichPresenceAssetSmall('discordsmall')
-        --SetDiscordRichPresenceAssetSmallText('https://discord.gg/cooley')
-
-        --loop for rich presense change
-
-         SetRichPresence('Players: ' .. NumberOfPlayers)
-         Wait(5*1000)
-         SetRichPresence('ID: ' .. id)
-         Wait(5*1000)
-         SetRichPresence('IGN: ' .. name)
-         Wait(5*1000)
-         SetRichPresence('Mafiacity ss1')
-         Wait(7*1000)
-		SetDiscordRichPresenceAction(1, "Join Discord", "https://discord.gg/ejqqUB9SBT")
-
-        --updates every selected seconds 
-        Citizen.Wait(20*1000)
-    end
+        SetRichPresence('player ='..onlinePlayers)
+        Citizen.Wait(10*1000)
+        SetRichPresence('name ='..name)
+		Citizen.Wait(10*1000)
+	end
 end)
